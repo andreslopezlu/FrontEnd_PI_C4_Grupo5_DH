fffafe7fb05f89c970cf2a603ec9f53c52a62a74
sergiosarmient0ing <sergiosarmient0ing@gmail.com>
2023-10-30 20:48:25 -0500

    agregue un texto informativo en la pagina de contacto

--- src/routes/Contacto.jsx
+++ src/routes/Contacto.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Contacto() {
+
+function Contact() {
   return (
-    <div>Contacto</div>
-  )
+    <div className="contact-container">
+      <h2 className="contact-heading">Contacto</h2>
+      <p className="contact-info">¡Gracias por visitar nuestra página de contacto!</p>
+      <p className="contact-info">Por favor, siéntete libre de contactarnos:</p>
+      <ul className="contact-info">
+        <li>Correo Electrónico: contacto@tudominio.com</li>
+        <li>Teléfono: (123) 456-7890</li>
+        <li>Dirección: 123 Calle Principal, Ciudad</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Contacto
+export default Contact;
