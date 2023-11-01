12f96ae0ec1f18b1be174d3a64a30e9c806ee124
nahuel <nahuel.ivaan@gmail.con>
2023-11-01 20:57:27 -0300

    Listar productos en tabla

--- src/routes/Administrador.jsx
+++ src/routes/Administrador.jsx
@@ -270,18 +270,28 @@ function Administrador() {
                 </button>
             </div>
 
-            <div className='homeCardCategorias homeCardProductos'>
-                {mostrarProductos && recomendados.map(props => (
-                    <CardProducto
-                        key={props.id}
-                        img={props.img}
-                        name={props.name}
-                        precio={props.precio}
-                        mostrarBotonAlquilar={false}
-                        mostrarBotonEliminar={true}
-                    />
-                ))}
-            </div>
+            {mostrarProductos && (
+                <div>
+                    <table className='productList'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Precio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {recomendados.map((producto) => (
+                                <tr key={producto.id}>
+                                    <td>{producto.id}</td>
+                                    <td>{producto.name}</td>
+                                    <td>{producto.precio}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
 
         </div>
     )
