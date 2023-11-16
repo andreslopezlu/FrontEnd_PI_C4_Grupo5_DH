b814a1cb10eae658f081ba98384c187248c80df0
sergiosarmient0ing <sergiosarmient0ing@gmail.com>
2023-11-15 22:10:48 -0500

    se agrego el boton de favorito a los productos del home

--- src/componets/CardProducto.jsx
+++ src/componets/CardProducto.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function CardProducto(props) {
+  const [favorito, setFavorito] = useState(false);
 
+  const toggleFavorito = () => {
+    setFavorito(!favorito);
+  };
 
-    return (
-        <div className='CardProducto'>
-            <div className='CardCProductoImg'>
-                <img src={`../imagenes/herramientas/${props.img}`} alt={props.name} />
-            </div>
-            <div className='CardProductoInfo'>
-                <h5>{props.name}</h5>
-                <p>{props.precio} <span>COP / Día</span></p>
-                {props.mostrarBotonAlquilar && <Link to={`/producto/${props.id}`}><button className='boton'>ALQUILAR</button></Link>}
-                {props.mostrarBotonEliminar && <button className='boton'>Eliminar</button>}
-            </div>
-        </div>
-    )
+  return (
+    <div className='CardProducto'>
+      <div className='CardFavoritoIcon' onClick={toggleFavorito}>
+      <FontAwesomeIcon icon={faStar} color={favorito ? 'gold' : 'white'} size="1.5x" />
+      </div>
+      {/* Imagen del producto */}
+      <div className='CardCProductoImg'>
+        <img src={`../imagenes/herramientas/${props.img}`} alt={props.name} />
+      </div>
+      {/* Información del producto */}
+      <div className='CardProductoInfo'>
+        <h5>{props.name}</h5>
+        <p>{props.precio} <span>COP / Día</span></p>
+        {/* Botón de Alquilar */}
+        {props.mostrarBotonAlquilar && <Link to={`/producto/${props.id}`}><button className='boton'>ALQUILAR</button></Link>}
+        {/* Botón de Eliminar */}
+        {props.mostrarBotonEliminar && <button className='boton'>Eliminar</button>}
+      </div>
+    </div>
+  );
 }
 
-export default CardProducto
+export default CardProducto;
+
